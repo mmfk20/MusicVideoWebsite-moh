726c052b3d495a7669f853a365ab93b954064113
mmfk20 <mmfkr9@hotmail.com>
2025-08-10 12:36:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const box = document.getElementById("animatedBox");
-  
+
   box.addEventListener("click", () => {
     const vw = window.innerWidth;
     const vh = window.innerHeight;
@@ -15,4 +15,30 @@ document.addEventListener("DOMContentLoaded", () => {
     box.style.left = randomLeft + "px";
     box.style.top = randomTop + "px";
   });
+
+  // Moving microphone animation
+  const mic = document.getElementById("mic");
+  let posX = 0;
+  let posY = 0;
+  let speedX = 2;
+  let speedY = 1.5;
+
+  function moveMic() {
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    posX += speedX;
+    posY += speedY;
+
+    // Bounce off edges
+    if (posX > vw - mic.offsetWidth || posX < 0) speedX *= -1;
+    if (posY > vh - mic.offsetHeight || posY < 0) speedY *= -1;
+
+    mic.style.left = posX + "px";
+    mic.style.top = posY + "px";
+
+    requestAnimationFrame(moveMic);
+  }
+
+  moveMic();
 });
